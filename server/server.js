7c7bd6010e142ed02a98f2707bc6b0b9762f71c5
Nhatty T.W <nhattytw@outlook.com>
2022-12-24 19:34:36 +0300

    New Packages + routes added

--- server/server.js
+++ server/server.js
@@ -15,19 +15,24 @@ app.use(cors())
 //Routes
 const auth = require('./routes/auth')
 const user = require('./routes/user')
+const zoom = require('./routes/zoom')
+const issue = require('./routes/issue')
+const project = require('./routes/project')
 const wbs = require('./routes/wbs')
-const issue = require('./routes/Issue')
-const project = require('./routes/Project')
+const item = require('./routes/item')
 const teams = require('./routes/teams')
 
 app.use('/api', auth)
 app.use('/api', user)
+app.use('/api', zoom)
 app.use('/api',wbs)
 app.use('/api',issue)
 app.use('/api',project)
 app.use('/api',teams)
+app.use('/api', item)
+
 //Backend server 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`)
