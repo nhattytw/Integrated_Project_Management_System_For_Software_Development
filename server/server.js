a07e693c37f24161c2b0789912c74c8b72dcefb3
nhattytw <nhatty567@outlook.com>
2023-01-11 22:26:00 +0300

    json parse fix

--- server/server.js
+++ server/server.js
@@ -9,24 +9,24 @@ app.use(express.json())
 app.use(cors())
 
 //Routes
-const auth = require("./routes/auth");
-const user = require("./routes/user");
-const zoom = require("./routes/zoom");
-const issue = require("./routes/issue");
-const project = require("./routes/project");
-const wbs = require("./routes/wbs");
-const teams = require("./routes/teams");
+const auth = require('./routes/auth')
+const user = require('./routes/user')
+const zoom = require('./routes/zoom')
+const issue = require('./routes/issue')
+const project = require('./routes/project')
+const wbs = require('./routes/wbs')
+const teams = require('./routes/teams')
 
-const tasks = require("./routes/tasks");
+const tasks = require('./routes/tasks')
 
-app.use("/api", auth);
-app.use("/api", user);
-app.use("/api", zoom);
-app.use("/api", wbs);
-app.use("/api", issue);
-app.use("/api", project);
-app.use("/api", teams);
-app.use("/api", tasks);
+app.use('/api', auth)
+app.use('/api', user)
+app.use('/api', zoom)
+app.use('/api', wbs)
+app.use('/api', issue)
+app.use('/api', project)
+app.use('/api', teams)
+app.use('/api', tasks)
 
 //Backend server
 const PORT = process.env.PORT || 5000
